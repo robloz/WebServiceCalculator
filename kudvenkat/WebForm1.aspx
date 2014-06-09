11377279e7fa7f2c7df43779e4ae6fbca91b307d
robloz <robloz87@gmail.com>
2014-06-09 23:31:53 +0100

    Video 3 finish
    I've done until video number 3

---
+++ kudvenkat/WebForm1.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="kudvenkat.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table border="2">
+            <tr>
+                <td>
+                    <asp:Label ID="LabelFirstNumber" runat="server" Text="First Number"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextFirstNumber" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="LabelSecondNumber" runat="server" Text="Second Number"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextSecondNumber" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="2">
+                    <asp:Label ID="LabelResult" runat="server" Text="Result Number"></asp:Label>
+                </td>
+
+            </tr>
+
+            <tr>
+                <td colspan="2">
+                    <asp:Button ID="ButtonAdd" runat="server" Text="+" OnClick="ButtonAdd_Click" />
+                    <asp:Button ID="ButtonSub" runat="server" Text="-" OnClick="ButtonSub_Click" style="width: 18px" />
+                    <asp:Button ID="ButtonMul" runat="server" Text="*" OnClick="ButtonMul_Click" />
+                    <asp:Button ID="ButtonDiv" runat="server" Text="/" OnClick="ButtonDiv_Click" />
+                </td>
+            </tr>
+        </table>
+
+
+
+
+    </div>
+        <asp:GridView ID="GVCalculation" runat="server"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ kudvenkat/WebServiceCalculator.asmx.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace kudvenkat
+{
+    /// <summary>
+    /// Summary description for WebServiceCalculator
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/WebServiceCalculator")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServiceCalculator : System.Web.Services.WebService
+    {
+        public enum EnumOperationBi { Add, Sub, Mul, Div };
+
+        public struct DataOperation
+        {
+            public float firstNumber;
+            public float secondNumber;
+            public float result;
+            public EnumOperationBi enumOper;
+        }
+
+       
+       // enum EnumOperationUn { ToThink };
+
+        List<Func<float, float, float>> l_operBi = new List<Func<float,float,float>>();
+       // List<Func<>>
+
+        public WebServiceCalculator()
+        {
+
+            l_operBi = new List<Func<float, float, float>>(
+                
+               // Enum.GetNames(typeof(EnumOperationBi)).Length
+                
+               new Func<float, float, float>[
+                   Enum.GetNames(typeof(EnumOperationBi)).Length  // la reserva de memoria se hace de esta forma
+                   ]
+
+                );
+
+
+
+            Console.WriteLine(l_operBi.Count);
+
+            l_operBi[(int)EnumOperationBi.Add] = delegate(float firstNumber, float secondNumber) { return firstNumber + secondNumber; };
+            l_operBi[(int)EnumOperationBi.Sub] = delegate(float firstNumber, float secondNumber) { return firstNumber - secondNumber; };
+            l_operBi[(int)EnumOperationBi.Mul] = delegate(float firstNumber, float secondNumber) { return firstNumber * secondNumber; };
+            l_operBi[(int)EnumOperationBi.Div] = delegate(float firstNumber, float secondNumber) { return firstNumber / secondNumber; };
+        
+        
+        
+        }
+
+        private void AddOperationInLog(float firstNumber, float secondNumber, float result, EnumOperationBi operation)
+        {
+            DataOperation dataOperation;
+            List<DataOperation> list_log = (List<DataOperation>)Session["LogOperations"];
+
+            if (list_log == null)
+            {
+                Session["LogOperations"] = new List<DataOperation>();
+                list_log = (List<DataOperation>)Session["LogOperations"];
+            }
+
+            dataOperation.firstNumber = firstNumber;
+            dataOperation.secondNumber = secondNumber;
+            dataOperation.result = result;
+            dataOperation.enumOper = operation;
+
+            list_log.Add(dataOperation);
+        }
+
+        private List<string> GetListStringLog()
+        {
+            List<DataOperation> list_log = (List<DataOperation>)Session["LogOperations"];
+
+            if (list_log == null)
+            {
+                List<string> l = new List<string>();
+                l.Add("No operations");
+                return l;
+            }
+            else
+            {
+                List<string> l_stringLog = new List<string>();
+
+                foreach (DataOperation dataLog in list_log)
+                {
+                    string operation_st = "";
+                    switch (dataLog.enumOper)
+                    {
+                        case EnumOperationBi.Add:
+                            operation_st = "+";
+                            break;
+                        case EnumOperationBi.Sub:
+                            operation_st = "-";
+                            break;
+                        case EnumOperationBi.Mul:
+                            operation_st = "*";
+                            break;
+                        case EnumOperationBi.Div:
+                            operation_st = "/";
+                            break;
+                    }
+
+                    l_stringLog.Add(dataLog.firstNumber.ToString() + " " + operation_st + " " + dataLog.secondNumber.ToString() + " = " + dataLog.result.ToString());
+                }
+
+                return l_stringLog;
+
+            }
+
+        }
+
+        /*
+A Web service is a method of communication between two electronic devices over a network. This 2 devices do not share sessions or any other server information besides those expilicitly defined by the web service signature.
+
+You have to remember that any device can access you web service and it this access should not depends on the language the it is using.
+
+As a result, you should add all the information you want to share via in the response you give.
+         **/
+
+        // Web services are stateless or everytime you call web service it will create a new instance of it. That's why its not allowed to use session in constructor.
+
+        [WebMethod(EnableSession=true)]
+        public float ExecuteOperation(float firstNumber, float secondNumber, EnumOperationBi operation)
+        {
+            float result = l_operBi[(int)operation]( firstNumber, secondNumber);
+            AddOperationInLog(firstNumber, secondNumber, result, operation);
+
+            return result;
+        }
+
+        [WebMethod(EnableSession = true)]
+        public int Add(int firstNumber, int secondNumber)
+        {
+            float result = firstNumber + secondNumber;
+            AddOperationInLog(firstNumber, secondNumber, result, EnumOperationBi.Add);
+
+            return (int)result;
+        }
+
+        [WebMethod(EnableSession = true)]
+        public List<string> GetLogOperations()
+        {
+            return GetListStringLog();
+
+        }
+
+    }
+}
